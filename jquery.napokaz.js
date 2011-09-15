5f040c684229c3ecff427d09be30b6bdaa8dd413
Grisha K <naspeh@gmail.com>
2011-09-15 19:40:42 +0300

    Updated markup of photo item;

--- jquery.napokaz.js
+++ jquery.napokaz.js
@@ -20,14 +20,15 @@
                 dataType: 'jsonp',
                 success: function(data) {
                     var items = [];
-                    $(data).find('media\\:group').each(function() {
+                    $(data).find('entry').each(function() {
                         var $this = $(this);
                         var item = {
-                            'orig': $this.find('media\\:content').attr('url'),
-                            'thumb': $this.find('media\\:thumbnail').attr('url'),
+                            'orig': $this.find('media\\:group media\\:content').attr('url'),
+                            'thumb': $this.find('media\\:group media\\:thumbnail').attr('url'),
+                            'picasa': $this.find('link[rel="alternate"]').attr('href'),
                             'thumbsize': opts.thumbsize
                         };
-                        item = getThumbItem(item);
+                        item = getThumbStr(item);
                         items.push(item);
                     });
                     container.append(items.join(''));
@@ -40,14 +41,16 @@
     };
     $.fn.napokaz.defaults = defaults;
 
-    function getThumbItem(params) {
+    function getThumbStr(params) {
         var str = (
-            '<a class="napokaz-thumb" href="<%= orig %>">' +
-            '   <span class="napokaz-thumb-inner"  style="' +
-            '       background-image: url(<%= thumb %>);' +
-            '       width: <%= thumbsize %>px; height: <%= thumbsize %>px;' +
-            '   ">&nbsp;</span>' +
-            '</a>'
+            '<div class="napokaz-item">' +
+            '   <a class="napokaz-thumb" href="<%= orig %>">' +
+            '       <img src="<%= thumb %>" width="<%= thumbsize %>" height="<%= thumbsize %>" />' +
+            '   </a>' +
+            '   <div class="napokaz-info">' +
+            '       <a href="<%= picasa %>">Посмотерть в picasa</a>' +
+            '   </div>' +
+            '</div>'
         );
         return _.template(str, params);
     }
